27d810381e66c301d78015cbe4af0a01a46c78af
dynann <dynannran@gmail.com>
2024-04-14 08:32:57 +0700

    second commit

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
    path('/new', PostCreateView.as_view(), name='post_new'),
    path('<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
    path('<int:pk>/edit/', PostUpdateView.as_view(), name='post_edit'),
-   path('/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
+   path('<int:pk>/', PostDetailView.as_view(), name='post_detail'),
 ]  
--- post/views.py
+++ post/views.py
@@ -1,14 +1,79 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView, FormView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.detail import SingleObjectMixin
+from .form import CommentForm
 from .models import Post
+from django.views import View
 # Create your views here.
 class PostView(ListView):
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
     
-class PostCreateView(CreateView):
+    
+class CommentGet(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+    context_object_name = 'post'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['form'] = CommentForm
+        return context 
+    
+class CommentPost(SingleObjectMixin, FormView):
+    model = Post
+    form_class = CommentForm
+    template_name = 'post_detail.html'
+    
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.post = self.object
+        comment.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        post = self.get_object()
+        return reverse("post_detail", kwargs={"pk": post.pk})
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ('title', 'body')
+    template_name = 'post_edit.html'
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user #type: ignore
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('post')
+    
+    def test_func(self):
+        obj = self.get_object()
+        return obj.author == self.request.user #type: ignore
+    
+class PostDetailView(LoginRequiredMixin, DetailView):
+    
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view() 
+        return view(request, *args, **kwargs)
+    
+       
+    
+class PostCreateView(LoginRequiredMixin ,CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ('title', 'body')
@@ -18,16 +83,3 @@ class PostCreateView(CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
-class PostUpdateView(UpdateView):
-    model = Post
-    fields = ('title', 'body')
-    template_name = 'post_edit.html'
-    
-class PostDeleteView(DeleteView):
-    model = Post
-    template_engine = 'post_delete.html'
-    success_url = reverse_lazy('post')
-    
-class PostDetailView(DetailView):
-    model = Post
-    template_name = 'post_detail.html'
